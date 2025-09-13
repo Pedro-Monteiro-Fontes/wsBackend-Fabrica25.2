f569900d27d4174f067ed13a0f43d30545b9f09f
Pedro-Monteiro-Fontes <pedromonteirofontes1@gmail.com>
2025-09-13 02:16:04 -0300

    chore(urls): adicionar rotas principais e auth

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Team, PlayerProfile
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (None, {'fields': ('role',)}),
+    )
+
+admin.site.register(Team)
+admin.site.register(PlayerProfile)
---
+++ app/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User, PlayerProfile
+
+class CoachUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username','email','role')  # role será 'coach' ao criar
+
+class PlayerUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username','email','role')  # role setado para 'player'
+
+class PlayerProfileForm(forms.ModelForm):
+    class Meta:
+        model = PlayerProfile
+        fields = ('team','photo','points_per_game','assists','rebounds','minutes')
